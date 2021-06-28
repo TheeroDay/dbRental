9d656050ddeb3d835bce58db797fc08e2cc3fb5f
TheeroDay <86235370+TheeroDay@users.noreply.github.com>
2021-06-28 07:18:58 +0700

    Delete WinFormsApp1/WinFormsApp1 directory

--- WinFormsApp1/WinFormsApp1/Global/ModObjekdanFungsi.vb
+++
@@ -1,7 +0,0 @@
-﻿Module ModObjekdanFungsi
-    Public KontrolLogin As New ClsCtlLogin
-    Public EntitasLogin As New ClsEntLogin
-
-    Public KodeLog As String
-    Public NameLog As String
-End Module
